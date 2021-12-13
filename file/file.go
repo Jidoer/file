46b28e8c0723256405909a6a65b78741158648e6
Jome Li <52993727+kangc666@users.noreply.github.com>
2021-12-13 21:49:51 +0800

    Create file.go

---
+++ file/file.go
@@ -0,0 +1,32 @@
+package file
+import (
+	"io/ioutil"
+	"os"
+)
+
+func Create(fileName string,nr string) {
+   content := []byte(nr)
+   err := ioutil.WriteFile(fileName, content, 0644)
+   if err != nil {
+      panic(err)
+   }
+}
+func Reader(fileName string) string{
+    
+   file, err := os.Open(fileName)
+   if err != nil {
+      panic(err)
+   }
+   defer file.Close()
+   content, err := ioutil.ReadAll(file)
+  // fmt.Println(string(content))
+  result := string(content)
+   return result
+}
+func Exists(path string) bool {
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
